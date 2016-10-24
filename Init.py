166ebfb7a1f9f6e93afe78a6a61cc8613ad344be
JackSpera[ITA] <giovanni.spera2011@libero.it>
2016-10-24 21:50:58 +0200

    Updated For Heroku

--- Init.py
+++ Init.py
@@ -1,4 +1,8 @@
 from Social import app, cfg
 import os
+HEROKU=False
 
-app.run(host=cfg["Host"], port=int(os.environ.get("PORT")), debug=cfg["Debug"])
+if HEROKU:
+  app.run(host=cfg["Host"], port=int(os.environ.get("PORT")), debug=cfg["Debug"])
+else:
+  app.run(host=cfg["Host"], port=cfg["Port"], debug=cfg["Debug"])
