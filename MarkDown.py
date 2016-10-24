7784b7db70c55290dc7bd21d72da905fbdefb8f7
JackSpera[ITA] <giovanni.spera2011@libero.it>
2016-10-24 21:53:13 +0200

    Delete MarkDown.py

--- MarkDown.py
+++
@@ -1,7 +0,0 @@
-import re
-p = re.compile(ur'#(\S{0,})', re.MULTILINE | re.IGNORECASE)
-test_str = u"#Ciao come va test prova #Bene"
-subst = u"<a href=\"/search/HashTag\g<1>\">\g<1></a>"
-
-result = re.sub(p, subst, test_str)
-print result
